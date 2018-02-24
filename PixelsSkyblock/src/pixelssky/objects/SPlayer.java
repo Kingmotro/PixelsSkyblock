dfc2250c058a7c6d78359fb67d71504d347f6c3e
Unknown <aaa49aaa49@gmail.com>
2018-02-24 15:06:03 +0100

    Début de SPlayers (besoin info SQL)

--- PixelsSkyblock/src/pixelssky/objects/SPlayer.java
+++ PixelsSkyblock/src/pixelssky/objects/SPlayer.java
@@ -2,59 +2,82 @@ package pixelssky.objects;
 
 import java.util.ArrayList;
 import pixelssky.utils.StringConverter;
+import java.sql.*;
 
-/*AKEKOUKOU*/
-
-public class SPlayer{
+public class SPlayer {
 	private String UUID;
 	private ArrayList<Data> data;
 	private ArrayList<Right> rights;
 	private Island island;
 	private ScoreboardObject sb;
-	
-	//TODO : Get and set
-	
-	SPlayer(String UUID)
-	{
-		//TODO : Load or create player data
+
+	// TODO : Get and set
+
+	// Database
+	static final String DB_URL = "";
+	static final String USER = "username";
+	static final String PASSWORD = "password";
+
+	// Connection
+	Connection conn;
+	Statement stmt;
+
+	SPlayer(String UUID) {
+		// TODO : Load or create player data
+		conn = null;
+		stmt = null;
 	}
-	
-	public void saveData(){
-		//TODO : Save all player data, UUID, Island ID and rights
-		//Note : Data has to be saved in string
-		//Note : Rights has to be saved in string
-		
-		for(Data d: data){
-			
-		}		
-		for(Right r: rights){
-			
+
+	public void saveData() {
+		// TODO : Save all player data, UUID, Island ID and rights
+		// Note : Data has to be saved in string
+		// Note : Rights has to be saved in string
+
+		String sql = "INSERT INTO";
+
+		for (Data d : data) {
+
 		}
-		
+		for (Right r : rights) {
+
+		}
+
+		try {
+
+			// Connection
+			conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+			stmt = conn.createStatement();
+			
+			// Request
+			stmt.executeQuery(sql);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	public String getUUID(){
+
+	public String getUUID() {
 		return UUID;
 	}
-	
-	public Data getData(String dataName)
-	{
-		for(Data d:data){
-			if(d.getDataName().equals(d))
-			{
+
+	public Data getData(String dataName) {
+		for (Data d : data) {
+			if (d.getDataName().equals(d)) {
 				return d;
 			}
 		}
 		return null;
 	}
-	
-	public void addOrSetData(String dataName, Object data){
+
+	public void addOrSetData(String dataName, Object data) {
 		Data d = getData(dataName);
-		if(d == null){
+		if (d == null) {
 			this.data.add(new Data(dataName, data));
-		}else{
+		} else {
 			d.setData(data);
 		}
 	}
-	
-	
+
 }
