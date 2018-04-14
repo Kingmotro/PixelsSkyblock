c11e085cd6ab671b6e1432cd09c5b706c13d2b6d
Unknown <nexfoxb5@gmail.com>
2018-04-14 18:16:22 +0200

    Correction fly

--- PixelsSkyblock/src/pixelssky/commands/FlyCommand.java
+++ PixelsSkyblock/src/pixelssky/commands/FlyCommand.java
@@ -12,12 +12,20 @@ public class FlyCommand implements CommandExecutor{
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
 		if (arg0 instanceof Player){
 			Player p = (Player) arg0;
-			if(arg3.length == 2){
+			if(arg3.length == 1){
 				p = Bukkit.getPlayer(arg3[1]);
 			}
-			if(arg3.length >= 1){
+			if(p.getAllowFlight()){
+				p.setAllowFlight(false);
+				p.sendMessage("§eVous ne pouvez plus voler ... comme le §adodo !");
+				p.sendMessage("§7https://fr.wikipedia.org/wiki/Dodo_(oiseau)");
+			}else{
 				p.setAllowFlight(true);
+				p.sendMessage("§eVous pouvez voler comme §asuperman !");
+				p.sendMessage("§7https://fr.wikipedia.org/wiki/Superman");
 			}
+			
+			
 		}
 		return true;
 	}
